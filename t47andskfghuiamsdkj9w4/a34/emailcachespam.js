59d47a1c83c822d2f9d5e2a15ac06017e696bd55
jeffmeng <jkmexpress@yahoo.com>
2016-05-16 04:34:52 +0000

    t

--- t47andskfghuiamsdkj9w4/a34/emailcachespam.js
+++ t47andskfghuiamsdkj9w4/a34/emailcachespam.js
@@ -67,7 +67,7 @@ function login(username, password) {
     
         
 
-        $("#password-list").append('<tr><th>' + response.website + '</th><th>' + username + '</th><th><span class="password hidden" id="' + response.website + '-password">' + password + '</span><span id="' + response.website + '-dots">******</span></th><th><a onclick="show(' + response.website + ')" onrelease="hide(' + response.website + ')" id="' + response.website + '-visibility">Show</a><a onclick="edit(' + response.website + ')">edit</a><a onclick="del(' + response.website + ')">delete</a></th></tr>');     
+        $("#password-list").append('<tr><th>' + response.website + '</th><th>' + username + '</th><th><span class="password hidden" id="' + response.website + '-password">' + password + '</span><span id="' + response.website + '-dots">******</span></th><th><a onclick="show(\'' + response.website + '\')" onrelease="hide(\'' + response.website + '\')" id="' + response.website + '-visibility">Show</a><a onclick="edit(\'' + response.website + '\')">edit</a><a onclick="del(\'' + response.website + '\')">delete</a></th></tr>');     
     }
 }
 function show(website) {
