80086663773b858e45aed3e1b9f73a3b282792a2
jeffmeng <jkmexpress@yahoo.com>
2016-05-16 04:28:51 +0000

    t

--- t47andskfghuiamsdkj9w4/a34/emailcachespam.js
+++ t47andskfghuiamsdkj9w4/a34/emailcachespam.js
@@ -52,8 +52,7 @@ function login(username, password) {
             console.log(response.userpass);
             //decrypted = sjcl.decrypt(masterPassword, response.userpass);
     
-            decrypted = sjcl.decrypt(masterPassword, JSON.stringify(response.userpass));
-            console.log(decrypted);
+            decrypted = atob(sjcl.decrypt(masterPassword, JSON.stringify(response.userpass)));
         //    showLogIn();
         } catch (e) {
             if (!reloading) {
@@ -63,9 +62,9 @@ function login(username, password) {
             }
         }
         
-        var username = window.atob(decrypted.split(',')[0]);
-        var password = window.atob(decrypted.split(',')[1]);
-        
+        var username = decrypted.split(',')[0];
+        var password = decrypted.split(',')[1];
+    
         
 
         $("#password-list").append('<tr><th>' + response.website + '</th><th>' + username + '</th><th><span class="password hidden" id="' + response.website + '-password">' + password + '</span><span id="' + response.website + '-dots">******</span></th><th><a onclick="show(' + response.website + ')" onrelease="hide(' + response.website + ')" id="' + response.website + '-visibility">Show</a><a onclick="edit(' + response.website + ')">edit</a><a onclick="del(' + response.website + ')">delete</a></th></tr>');     
